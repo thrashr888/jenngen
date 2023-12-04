e62f3636942b8ea026702d63a47d72de79a659ba
Paul Thrasher <thrashr888@gmail.com>
2023-12-03 17:59:18 -0800

    gen buffer tweaks

--- index.mjs
+++ index.mjs
@@ -19,23 +19,26 @@ const CACHE_DIR = ".jenngen/cache";
 const openai = new OpenAI();
 
 const ASSISTANT_PROMPT = `
-You are JennGen, an AI static site generator. Your task is to transform given pseudo-code or instructions into high-quality, deployable code, matching the content and file name specifications.
+You are JennGen, an AI-driven static site generator. Your role is to convert provided pseudo-code or instructions into high-quality, deployable code, tailored to the file name and content specifications. The output should be similar to a $35,000 website from a top-tier web development agency using the latest development practices.
 
-Input File Contents:
-- The input may include mixed content types, like markdown with JavaScript, or prose with CSS.
-- Text within quotes should be used verbatim. E.g., "Insert privacy policy here" means inserting a standard privacy policy.
-- Outside of quotes, generate professional-grade code or copy suitable for the type of file.
-- Eliminate all pseudo-code or placeholder content. The output must be real, deployable code or functional prose.
-- Avoid filler content like "lorem ipsum" or emojis, unless specifically requested.
+Input File Content Rules:
+- Mixed Content Types: Inputs may include combinations such as markdown with JavaScript, or prose with CSS, or instruction text mixed with literal quotes.
+- Literal Text: Text within double quotes ("") should be used exactly as is. For instance, "This is my description" translates directly into "This is my description".
+- Dynamic Placeholders: Text within curly braces ({}) represents dynamic variables. Replace {title} with the actual title of the page.
+- Instructional Text: Text within square brackets ([]) contains instructions to generate specific content. For example, [Insert a random number] should be replaced with an actual random number, and [Insert marketing copy here] should be replaced with professional marketing content.
+- Interactive Elements: Text within angle brackets (<>) should be replaced with interactive or real-time content. E.g., <Ask the user for their name> becomes a user input prompt for their name, and <clock with current time> becomes a live JavaScript clock showing the current time.
+- Professional Standard: All generated content should be professional-grade, appropriate for the file type, and devoid of any pseudo-code or placeholders.
+- No Filler Content: Avoid using "lorem ipsum", emojis, or similar filler material, unless explicitly requested.
 
 Output File Specifications:
-- The output code language should correspond to the file extension (e.g., .html for HTML, .js for JavaScript, .tf for Terraform, .css for CSS, Makefile for a valid Make file).
-- Ensure text has correct capitalization, spelling, and grammar.
-- For HTML, use style tags, inline CSS, and Tailwind (CDN url: https://cdn.tailwindcss.com).
-- Implement dynamic content as needed using JavaScript script tags.
-- All output code should be functional and ready for website deployment.
-- Provide only the code output, without markdown or explanatory prose.
-- The resulting website should have a nice, high quality design, good styling, with a consistent look and feel across pages.
+- Language Match: Output code should correspond to the file extension (e.g., .html for HTML, .js for JavaScript).
+- Text Quality: Ensure correct capitalization, spelling, and grammar.
+- HTML Styling: Use style tags, inline CSS, and Tailwind (CDN: https://cdn.tailwindcss.com).
+- HTML Quality: The webpage should be responsive, with good accessibility and semantic HTML for SEO.
+- Dynamic JavaScript: Implement interactive or dynamic content as needed using JavaScript.
+- Deployment-Ready: All output code must be functional and ready for website deployment.
+- Pure Code Output: Provide only the code output, without markdown code blocks or explanatory prose.
+- Design Quality: The output should feature an attractive, high-quality design with consistent styling across pages.
 
 User Instructions for the Website:
 <<<INSTRUCTIONS>>>
@@ -44,11 +47,9 @@ Current Folder Files:
 <<<FILES>>>
 
 Pseudo-Code Input Examples:
-===========================
 <<<INPUT>>>
 
 Real Code Output Examples:
-===========================
 <<<OUTPUT>>>
 `;
 
@@ -105,7 +106,8 @@ async function completion(assistant, prompt) {
       { role: "assistant", content: assistant },
       { role: "user", content: prompt },
     ],
-    model: "gpt-3.5-turbo",
+    // model: "gpt-3.5-turbo",
+    model: "gpt-4-1106-preview",
     stream: true,
     temperature: 0.5,
     top_p: 1,
@@ -166,7 +168,7 @@ function applyExamples(prompt, examples) {
     .replace("<<<OUTPUT>>>", outputs.join("\n"));
 }
 
-async function generateCode(assistantPrompt, file) {
+async function generateCode(assistantPrompt, file, liveReloadServer = null) {
   if (!file) return;
   if (!(await hasFileChanged(file))) {
     // console.log(chalk.blue(`Skipping ${file.path}`));
@@ -194,14 +196,51 @@ async function generateCode(assistantPrompt, file) {
 
   // create folder if it doesn't exist
   await fs.mkdir(path.dirname(distPath), { recursive: true });
-
   await fs.writeFile(distPath, ""); // create empty file
+
+  const CODE_BLOCK_REGEX = /^```[a-zA-Z]+\n/; // Matches code blocks like ```language
+  const END_BLOCK_REGEX = /\n```$/; // Matches closing code blocks
+  const BUFFER_SIZE = 100; // Adjust as needed
+  const END_BUFFER_SIZE = 10; // Size to capture the closing block
+  let buffer = "";
+  let endBuffer = "";
+  let isBufferProcessed = false;
+
   const fileStream = createWriteStream(distPath);
   for await (const chunk of completionStream) {
     if (typeof chunk.choices[0]?.delta?.content !== "string") continue;
-    fileStream.write(chunk.choices[0].delta.content);
+
+    if (!isBufferProcessed) {
+      buffer += chunk.choices[0].delta.content;
+      if (buffer.length >= BUFFER_SIZE) {
+        // Process buffer and write to file
+        const match = buffer.match(CODE_BLOCK_REGEX);
+        if (match) {
+          buffer = buffer.substring(match[0].length);
+        }
+        fileStream.write(buffer);
+        buffer = ""; // Clear buffer
+        isBufferProcessed = true;
+      }
+    } else {
+      endBuffer += chunk.choices[0].delta.content;
+      if (endBuffer.length > END_BUFFER_SIZE) {
+        // Write to file except for the last part reserved in endBuffer
+        fileStream.write(
+          endBuffer.substring(0, endBuffer.length - END_BUFFER_SIZE)
+        );
+        endBuffer = endBuffer.substring(endBuffer.length - END_BUFFER_SIZE);
+      }
+    }
   }
+
+  // Process the end buffer to remove the closing Markdown block
+  if (END_BLOCK_REGEX.test(endBuffer)) {
+    endBuffer = endBuffer.replace(END_BLOCK_REGEX, "");
+  }
+  fileStream.write(endBuffer);
   fileStream.end();
+  liveReloadServer?.refresh();
 
   console.log(chalk.green(`Generated ${distPath}`));
   await updateCache(file);
@@ -219,10 +258,12 @@ async function startServer() {
   const liveReloadServer = livereload.createServer({ port: LIVERELOAD_PORT });
   liveReloadServer.watch(path.join(process.cwd(), DIST_DIR));
 
-  watch(DIST_DIR, { recursive: true }, (_, filename) => {
+  watch(DIST_DIR, { recursive: true, persistent: true }, (_, filename) => {
     liveReloadServer.refresh(filename);
     console.log(chalk.magenta(`Reloaded due to change in ${filename}`));
   });
+
+  return [app, liveReloadServer];
 }
 
 async function main() {
@@ -262,14 +303,15 @@ async function main() {
 
   await fs.mkdir(CACHE_DIR, { recursive: true });
 
-  await build(sourceFolder, assistantPrompt);
-
+  let liveReloadServer;
   if (argv.server) {
-    await startServer();
+    const [app, liveReloadServer] = await startServer();
   }
 
+  await build(sourceFolder, assistantPrompt);
+
   if (argv.watch) {
-    await watchForChanges(sourceFolder, assistantPrompt);
+    await watchForChanges(sourceFolder, assistantPrompt, liveReloadServer);
   }
 }
 
@@ -298,7 +340,11 @@ async function build(sourceFolder, assistantPrompt) {
   }
 }
 
-async function watchForChanges(sourceFolder, assistantPrompt) {
+async function watchForChanges(
+  sourceFolder,
+  assistantPrompt,
+  liveReloadServer = null
+) {
   console.log(chalk.green("Watching for changes..."));
   watch(sourceFolder, { recursive: true }, async (_, filename) => {
     if (!filename) {
@@ -318,9 +364,8 @@ async function watchForChanges(sourceFolder, assistantPrompt) {
 
       // Check if the file has changed
       if (await hasFileChanged(file)) {
-        await generateCode(assistantPrompt, file);
-      } else {
-        console.log(chalk.blue(`No changes in ${filename}`));
+        await generateCode(assistantPrompt, file, liveReloadServer);
+        // if (liveReloadServer) liveReloadServer.refresh();
       }
     } catch (err) {
       console.error(chalk.red(`Generation failed ${filename}: ${err}`));
